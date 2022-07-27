ff9a1f77164b6b8b7a4d2bc6d2cf39c2d2e15193
lesiak <lesiak@outlook.com>
2022-07-27 14:41:03 +0200

    [APP1] Dynamic view logic

--- app1/monthly_challenges/challenges/views.py
+++ app1/monthly_challenges/challenges/views.py
@@ -1,15 +1,29 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+monthly_challenges = {
+    'january': 'January challenge',
+    'february': 'February challenge',
+    'march': 'March challenge',
+    'april': 'April challenge',
+    'may': 'May challenge',
+    'june': 'June challenge',
+    'july': 'July challenge',
+    'august': 'August challenge',
+    'september': 'September challenge',
+    'october': 'October challenge',
+    'november': 'November challenge',
+    'december': 'December challenge',
+}
+
 
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 
 def monthly_challenge(request, month):
-    if month == 'january':
-        return HttpResponse('January challenge!')
-    elif month == 'february':
-        return HttpResponse('February challenge')
-    else:
-        return HttpResponseNotFound("This month is not supported")
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except KeyError as E:
+        return HttpResponseNotFound(f"Month {E} is not supported")
