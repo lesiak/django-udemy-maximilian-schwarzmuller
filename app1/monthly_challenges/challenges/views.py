ba886c8907f6bc7f81ad9f8cbd12e858c3bd6e8b
lesiak <lesiak@outlook.com>
2022-07-28 09:27:55 +0200

    [APP1] The `reverse()` function & named URLs

--- app1/monthly_challenges/challenges/views.py
+++ app1/monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 monthly_challenges = {
     'january': 'January challenge',
@@ -21,7 +22,8 @@ def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     try:
         redirect_month = months[month - 1]
-        return HttpResponseRedirect('/challenges/' + redirect_month)
+        redirect_path = reverse('month-challenge', args=[redirect_month])
+        return HttpResponseRedirect(redirect_path)
     except IndexError:
         return HttpResponseNotFound(f"Month {month} is not supported")
 
