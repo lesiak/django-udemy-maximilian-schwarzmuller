483cc3ac90e6d0d638c0e047fb8b051ea04c6267
lesiak <lesiak@outlook.com>
2022-07-28 20:20:55 +0200

    [APP1] Index page

--- app1/monthly_challenges/challenges/views.py
+++ app1/monthly_challenges/challenges/views.py
@@ -18,6 +18,20 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    def list_item(month: str):
+        redirect_path = reverse('month-challenge', args=[month])
+        return f"<li><a href={redirect_path}>{month.capitalize()}</a></li>"
+    list_items = (list_item(month) for month in monthly_challenges.keys())
+    months_list = '\n'.join(list_items)
+    response_data = f"""
+    <ul>
+        {months_list}
+    </ul>
+    """
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     try:
