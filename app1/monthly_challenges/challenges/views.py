b41d2cfaa6ea367ec4a0d214e4ba1d96c537048b
lesiak <lesiak@outlook.com>
2022-07-29 13:31:52 +0200

    [APP1] `render` function

--- app1/monthly_challenges/challenges/views.py
+++ app1/monthly_challenges/challenges/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from django.urls import reverse
 
 monthly_challenges = {
@@ -47,7 +46,6 @@ def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
         # response_data = f"<h1>{challenge_text}</h1>"
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html")
     except KeyError as e:
         return HttpResponseNotFound(f"<h1>Month {month} is not supported</h1>")
